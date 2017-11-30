a052ed5b2301b945e7906084dfe198450c42b0a8
Andrew Warinner <andrew.warinner@magnolia-cms.com>
2017-11-30 11:58:22 +0100

    cleaned up imports

--- src/test/java/info/magnolia/services/subscriptiontools/commands/AddSubscriberCommandTest.java
+++ src/test/java/info/magnolia/services/subscriptiontools/commands/AddSubscriberCommandTest.java
@@ -31,7 +31,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
